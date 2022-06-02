065e2a5b584048ca5a21aab0409a9d1820369237
Tanaka  Khondowe <t.khondowe@gmail.coml>
2022-06-02 01:36:20 -0500

    App entry point

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    // BEM convention
+    <div className="app">
+      <Header />
+      {/* The Home Component */}
+    </div>
+  );
+}
+
+export default App;
