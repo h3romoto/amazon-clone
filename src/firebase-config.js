6b1ce47e24e55d0af8ecb1a9e8f3c3900990c36d
Tanaka  Khondowe <t.khondowe@gmail.coml>
2022-06-02 01:37:50 -0500

    firebase config

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCB-fyDjvHV6UPzss6RDZImTnpHkH43XiU",
+  authDomain: "clone-e178e.firebaseapp.com",
+  projectId: "clone-e178e",
+  storageBucket: "clone-e178e.appspot.com",
+  messagingSenderId: "944079759389",
+  appId: "1:944079759389:web:971eb8d3aa9fc62085bef4",
+  measurementId: "G-R9KEE383PD"
+};
