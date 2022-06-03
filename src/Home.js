28962296fbebb32402c3c59e6ff8e2ba34135047
Tanaka  Khondowe <t.khondowe@gmail.coml>
2022-06-03 11:24:43 -0400

    Home component with AMZ banner image

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,11 @@ import './Home.css'
 function Home() {
   return (
     <div className='home'>
-
+      <div className='home__container'>
+        <img className='home__image' 
+          src='https://m.media-amazon.com/images/I/A1o-1kou5CL._SX3000_.jpg' alt='' 
+        />
+      </div>
     </div>
   )
 }
