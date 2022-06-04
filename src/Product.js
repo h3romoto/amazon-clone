ad3d97b711e2e25734a88213016e10926703449f
Tanaka  Khondowe <t.khondowe@gmail.coml>
2022-06-04 10:46:11 -0400

    added a single product title, price, image, and rating

--- src/Product.js
+++ src/Product.js
@@ -5,12 +5,23 @@ function Product() {
   return (
     <div className='product'>
       <div className='product__info'>
-        <p>The Lean Startup</p>
+        <p>The lean startup</p>
         <p className='product__price'>
           <small>$</small>
-          <strong>2</strong>
+          <strong>19.99</strong>
         </p>
+        <div className='product__rating'> 
+          <p> &#9733;</p>
+          <p> &#9733;</p>
+          <p> &#9733;</p>
+          <p> &#9733;</p>
+        </div>
       </div>
+      <img src='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg' alt='' />
+
+      <button>
+        Add to bucket
+      </button>
     </div>
   )
 }
