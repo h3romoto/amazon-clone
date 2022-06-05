fdeba3db0e26196897ce5bd901939d85364b8bc7
Tanaka <t.khondowe@gmail.com>
2022-06-05 12:17:44 -0400

    properly nested Product component within the home container

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css'
+import Product from './Product'
 
 function Home() {
   return (
@@ -8,6 +9,24 @@ function Home() {
         <img className='home__image' 
           src='https://m.media-amazon.com/images/I/A1o-1kou5CL._SX3000_.jpg' alt='' 
         />
+
+        <div className='home__row'>
+          <Product />
+          {/* Product */}
+          {/* Product */}
+        </div>
+
+        <div className='home__row'>
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+
+        <div className='home__row'>
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
       </div>
     </div>
   )
