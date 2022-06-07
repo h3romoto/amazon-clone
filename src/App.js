ced729b5b7222276cd46b8affa482ceff9fc7f18
Tanaka <t.khondowe@gmail.com>
2022-06-07 09:50:59 -0500

    Added a route for the landing page. Used a fragment to render multiple HTML elements for Header and Home components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     // BEM convention
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<><Header /><Home /></>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
