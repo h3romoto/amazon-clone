02074506497bc0df64a9720b154019477da97dc8
Tanaka <t.khondowe@gmail.com>
2022-06-06 19:59:43 -0500

    arrow function for product rating stars

--- src/Product.js
+++ src/Product.js
@@ -6,13 +6,17 @@ function Product({ title, image, price, rating}) {
   return (
     <div className='product'>
       <div className='product__info'>
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className='product__price'>
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className='product__rating'> 
-          <p> &#9733; &#9733; &#9733; &#9733;</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+            <p> &#9733;</p>
+          ))}
         </div>
       </div>
       <img src='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg' alt='' />
