d2893619d2f9814e9a2e2af7ce6626b80c973f25
Tanaka <t.khondowe@gmail.com>
2022-06-06 19:50:00 -0500

    Added product info vars

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './Product.css'
 
-function Product() {
+//use props for different products
+function Product({ title, image, price, rating}) {
   return (
     <div className='product'>
       <div className='product__info'>
