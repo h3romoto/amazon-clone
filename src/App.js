daa4e9f574ad12e02ac9b21f79a688a2770b8306
Tanaka <t.khondowe@gmail.com>
2022-06-07 09:29:46 -0500

    import react-router-don

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header';
+import Home from './Home';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     // BEM convention
     <div className="app">
       <Header />
-      {/* The Home Component */}
+      <Home />
     </div>
   );
 }
