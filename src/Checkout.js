3c08f8f1f95a28b45d7acc731a60d317fc43866a
Tanaka <t.khondowe@gmail.com>
2022-06-08 18:29:48 -0500

    Added the Checkout component

---
+++ src/Checkout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Checkout() {
+  return (
+    <div className='checkout__container'>
+      <p>
+        This is the checkout page!
+      </p>
+    </div>
+  )
+}
+
+export default Checkout 
