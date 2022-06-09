373f0dfb8c5b9fa44a1f9c7e837d3f7a6e55d073
Tanaka <t.khondowe@gmail.com>
2022-06-08 19:05:09 -0500

    Checkout left ad section

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import './Checkout.css'
 
 function Checkout() {
   return (
     <div className='checkout__container'>
-      <p>
-        This is the checkout page!
-      </p>
+      <div className='checkout__left'>
+        <div className='checkout__ad'>
+          <img 
+            src='https://images-na.ssl-images-amazon.com/images/G/01/credit/img21/CBCC/banner-card-art/cbcc_prime_gray_naf-banner._CB627562113_.png'
+            alt='' 
+          />
+        </div>
+      </div>
+      <div className='checkout__right'>
+
+      </div>
     </div>
   )
 }
