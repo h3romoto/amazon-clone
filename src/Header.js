37dd95cb62948511bc94281b395ceb8f24b102f8
Tanaka <t.khondowe@gmail.com>
2022-06-08 19:20:39 -0500

    Made AMZ logo clickable link to home

--- src/Header.js
+++ src/Header.js
@@ -2,14 +2,17 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className='header'>
-      <img
-        className='header__logo'
-        src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
-      />
+      <Link to="/">
+        <img
+          className='header__logo'
+          src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
+        />
+      </Link>
 
       <div className='header__search'>
         <input 
