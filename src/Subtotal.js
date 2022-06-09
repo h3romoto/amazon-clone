550172ef1fa1b66cbf2c6bbfa657aab0b51b2ff4
Tanaka <t.khondowe@gmail.com>
2022-06-09 11:47:58 -0500

    Subtotal component with prop vars

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import './Subtotal.css'
+import CurrencyFormat from 'react-currency-format'
 
 function Subtotal() {
   return (
     <div className='subtotal__container'>
-      
+      <CurrencyFormat
+        renderText={(value) => (
+          // use HTML fragment for rendering multiple components via the same route
+          <>
+            <p>
+              Subtotal ({basket.length} items):
+              <strong>{` ${value}`}</strong>
+            </p>
+            <small className='subtotal__gift'>
+              <input type="checkbox">
+                This order contains a gift.
+              </input>
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={getBasketTotal(basket)}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
     </div>
-  )
+  );
 }
 
 export default Subtotal
