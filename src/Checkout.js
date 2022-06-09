2f6a4357ed813fed4b1b77a08ff9ba05e0cf4738
Tanaka <t.khondowe@gmail.com>
2022-06-09 11:19:57 -0500

    Add Subtotal component to Checkout

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Checkout.css'
+import Subtotal from './Subtotal'
 
 function Checkout() {
   return (
@@ -23,7 +24,7 @@ function Checkout() {
 
       <div className='checkout__right'>
         <h2>
-          Checkout subtotal will go here
+          <Subtotal />
         </h2>
       </div>
     </div>
