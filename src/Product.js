55b130606a30239f3bf012158b451688f5aefdfe
Tanaka <t.khondowe@gmail.com>
2022-06-12 09:02:32 -0500

    addToBasket method with dispatch

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import './Product.css'
+import { useStateValue } from './StateProvider'
 
 //use props for different products
 function Product({ title, id, image, price, rating}) {
+  const [state, dispatch] = useStateValue();
+
+  addToBasket = () => {
+    //dispatch item into the data layer
+    dispatch({
+      type: ADD_TO_BASKET,
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className='product'>
       <div className='product__info'>
@@ -21,7 +38,7 @@ function Product({ title, id, image, price, rating}) {
       </div>
       <img src={image} alt='' />
 
-      <button className='addTo__bucket'>
+      <button onClick={addToBasket} className='addTo__bucket'>
         Add to bucket
       </button>
     </div>
