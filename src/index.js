de3f0e9d65f2dea22abf4be1c48d91fc8be697da
Tanaka <t.khondowe@gmail.com>
2022-06-12 09:08:18 -0500

    imported initialState

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StateProvider } from './StateProvider';
+import reducer, {initialState} from './reducer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +13,6 @@ root.render(
     <StateProvider initialState={initialState} reducer={reducer}>
       <App />
     </StateProvider>
-
   </React.StrictMode>
 );
 
