cfe40de861017221f1389560a674388f41505b26
Tanaka <t.khondowe@gmail.com>
2022-06-13 12:28:43 -0500

    updated Checkout

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import './Checkout.css'
 import Subtotal from './Subtotal'
+import { useStateValue } from './StateProvider'
+import CheckoutProduct from './CheckoutProduct'
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className='checkout__container'>
       <div className='checkout__left'>
@@ -15,10 +19,18 @@ function Checkout() {
         <div>
           <h3 className='checkout__title'>
             Your Shopping Basket.
-          </h3>
-          {/* Basket item */}
-          {/* Basket item */}
-          {/* Basket item */}
+          </h3> 
+          {basket.map( item => (
+            <CheckoutProduct 
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            /> 
+          ))}
+
+          {/* CheckoutProduct */}
         </div>
       </div>
 
