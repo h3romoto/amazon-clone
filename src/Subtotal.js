c95c3c7f6229080f9e38c5f7d2bdfba323a450ca
Tanaka <t.khondowe@gmail.com>
2022-06-13 11:18:40 -0500

    subtotal amount component complete

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Subtotal.css'
 import CurrencyFormat from 'react-currency-format'
 import { useStateValue } from './StateProvider'
+import { getBasketTotal } from './reducer';
 
 function Subtotal() {
   const [{ basket }, dispatch] = useStateValue();
@@ -17,7 +18,7 @@ function Subtotal() {
           }
         }
     });
-    
+
     console.log("Basket total: ", basketTotal)
 
     dispatch ({
@@ -32,7 +33,7 @@ function Subtotal() {
           // <></> is a fragment for rendering multiple components via same route
           <>
             <p className='subtotal_items'>
-              Subtotal ({basket.length} items): <strong>{basketTotalValue}</strong>
+              Subtotal ({basket.length} items): <strong>{value}</strong>
             </p>
             <small className='subtotal__gift'>
               <input type="checkbox" />
@@ -41,7 +42,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0}
+        value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
