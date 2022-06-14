e1e4585a0472c11c268cb653ef7c89b88839a908
Tanaka <t.khondowe@gmail.com>
2022-06-14 09:58:15 -0500

    Added login route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import Login from './Login'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Checkout from './Checkout'
 
@@ -10,12 +11,12 @@ function App() {
     <Router>
       <div className="app">
         {/* render the Header always */}
-        <Header />
         <Routes>
           {/* use HTML fragment for rendering multiple components via the same route */}
-          <Route path="/checkout" element={<><Checkout /></>} />
+          <Route path="/login" element={<><Login /></>} />
+          <Route path="/checkout" element={<><Header /><Checkout /></>} />
           {/* make sure the home component is at the bottom */}
-          <Route path="/" element={<><Home /></>} />
+          <Route path="/" element={<><Header /><Home /></>} />
         </Routes>
       </div>
     </Router>
