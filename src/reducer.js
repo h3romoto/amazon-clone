d997c288948708a37127e3ea9466674df8efcada
Tanaka <t.khondowe@gmail.com>
2022-06-14 09:20:43 -0500

    Remove product from the basket complete

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,3 @@
-import { defaultListboxReducer } from "@mui/base";
-
 export const initialState = {
   basket: [], 
 };
@@ -18,10 +16,28 @@ const reducer = (state, action) => {
         basket: [...state.basket, action.item]
       };
 
+    case "REMOVE_FROM_BASKET":
+        const index = state.basket.findIndex(
+          (basketItem) => basketItem.id === action.id
+        );
+        let newBasket = [...state.basket];
+
+        if (index >= 0) {
+          newBasket.splice(index, 1)
+        } else {
+          console.warn(
+            `Can't remove product (id: ${action.id}) since it's not in the basket`
+          )
+        }
+
+        return {
+          ...state,
+          basket: newBasket
+        };
+
       default:
         return state;
   }
-
 }
 
 export default reducer;
