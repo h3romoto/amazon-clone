4c97f84881a4aa26fbd1e280cd2bbf668a650703
Tanaka <t.khondowe@gmail.com>
2022-06-14 11:21:05 -0500

    setMail, setPassword

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,46 @@
 import './Login.css'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const signIn = e => {
+    // prevent app from refreshing
+    e.preventDefault()
+  }
+
+const register = e => {
+  // prevent app from refreshing
+  e.preventDefault()
+}
+
   return (
-    <div className='login__container'>
+    <div className='login'>
       <Link to="/">
         <img
           className='login__logo'
           src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1200px-Amazon_logo.svg.png'
         />
       </Link>
+      <div className='login__container'>
+        <h1>Sign in</h1>
+        <form>
+          <h5>Email</h5>
+          <input type="text" value={email} onChange={e => setEmail(e.target.email)} />
+          <h5>Password</h5>
+          <input type="password" value={password} onChange={e => setEmail(e.target.password)}/>
+          <button type='submit' className='signIn__button' onClick={signIn}>Sign in</button>
+
+          <p>
+            By signing in, you are agreeing to the terms & conditions of this fake Amazon website.
+          </p>
+          <p className='accountSignUp__prompt'>
+            Don't have an account?
+          </p>
+          <button type='submit' className='signUp__button' onClick={register}>Create your Amazon Account</button>
+        </form>
+      </div>
     </div>
   )
 }
