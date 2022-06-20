bc0c2648982c276f6058f468bab4358013cdbce5
Tanaka <t.khondowe@gmail.com>
2022-06-19 21:59:15 -0500

    Added user email and address title to Payment component

--- src/Payment.js
+++ src/Payment.js
@@ -2,19 +2,27 @@ import React from 'react'
 import './Payment.css'
 
 function Payment() {
+  const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className='payment'>
       <div className='paymnent_container'>
         {/* Delivery address */}
         <div className='payment__section'>
-
+          <div className='payment__title'>
+            <h3>Delivery Address</h3>
+          </div>
+          <div className='payment__address'>
+            <p>
+              { user?.email }
+            </p>
+          </div>
         </div>
         {/* Review items */}
         <div className='payment__section'>
 
         </div>
         <div className='payment__section'>
-          
+
         </div>
         {/* Payment method */}
       </div>
