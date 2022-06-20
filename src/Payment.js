17a669b74f6a89f8839c63cece2838a9030932b1
Tanaka <t.khondowe@gmail.com>
2022-06-19 21:52:02 -0500

    Payment.js

---
+++ src/Payment.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Payment.css'
+
+function Payment() {
+  return (
+    <div className='payment'>
+      <div className='paymnent_container'>
+        {/* Delivery address */}
+        <div className='payment__section'>
+
+        </div>
+        {/* Review items */}
+        <div className='payment__section'>
+
+        </div>
+        <div className='payment__section'>
+          
+        </div>
+        {/* Payment method */}
+      </div>
+    </div>
+  )
+}
+
+export default Payment
