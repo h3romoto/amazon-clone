0f7e2ba5d680614570d1dd0ab64735431ff647e5
Tanaka <t.khondowe@gmail.com>
2022-06-22 19:18:04 -0500

    Handle errors

--- src/Payment.js
+++ src/Payment.js
@@ -93,6 +93,8 @@ function Payment() {
                         </span>
                     </button>
                   </div>
+                  {/* Errors */}
+                  { error && <div>{error}</div>}
               </form>
           </div>
         </div>
