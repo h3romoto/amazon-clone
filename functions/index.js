3028ff235b622e5d69c023e097fcfa4a251d64d9
Tanaka <t.khondowe@gmail.com>
2022-06-22 20:20:33 -0500

    Test api routes and add listener

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
+const { response } = require("express");
 const stripe = require("stripe")('sk_test_51LCiWcFdRVVKiVDJ2D1o9yyyRHd7ig4claIIOOxFq3c9YGfKrlyi21vZBXJ6ikp51ngCVPeMHIVAEhTFZZYeOSnE00UcGcFPdP')
 
 
@@ -14,5 +15,7 @@ app.use(cors({ origin: true }))
 app.use(express.json());
 
 // API routes
+app.get('/', (request, response) => response.status(200).send('Hello World'))
 
 // Listen command
+exports.api = functions.https.onRequest(app)
