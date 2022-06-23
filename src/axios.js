46798ffd83b339f7a17522afba5e92e9a385b86d
Tanaka <t.khondowe@gmail.com>
2022-06-23 07:39:02 -0500

    Cleaning up

--- src/axios.js
+++ src/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: 'http://localhost:5001/clone-e178e/us-central1/api'    // The API (cloud function) URL
-})
+});
 
 export default instance;
