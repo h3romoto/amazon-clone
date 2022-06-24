93718c0182baf570621bdf6802a18d0779ec7998
Tanaka <t.khondowe@gmail.com>
2022-06-23 19:23:08 -0500

    Empty basket dispatch

--- src/Payment.js
+++ src/Payment.js
@@ -53,6 +53,10 @@ function Payment() {
       setError(null);
       setProcessing(false);
 
+      dispatch({
+        type: 'EMPTY_BASKET'
+      })
+
       // supposed to be history.replace('/orders')? (but useHistory seems to be deprecated)
       navigate('/orders')
     })
