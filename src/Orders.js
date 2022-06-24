1d28418f0c33a2c7ba2150ea541c033b1489adf5
Tanaka <t.khondowe@gmail.com>
2022-06-23 20:04:40 -0500

    Useffect for collecting user order data from firestore

--- src/Orders.js
+++ src/Orders.js
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { db } from './firebaseconf';
+import { useStateValue } from './StateProvider'
 
 function Orders() {
+  const  [{ basket, user }, dispatch] = useStateValue();
+  const  [orders, setOrders] = useState([])
+  
+  useEffect(() => {
+    if (user) {
+      db
+      .collection('users')
+      .doc(user?.uid)
+      .orderBy('created', 'desc')
+      .onSnapshot(snapshot => (
+        setOrders(snapshot.docs.map(doc => ({
+          id: doc.id,
+          data: doc.data()
+        })))
+      )) 
+    } else {
+      setOrders([])
+    }
+
+  }, [user])
+
   return (
     <div className='orders'>
       <h1> Your Orders </h1>
