6bad32b8601f3f90faa656eef814eff61a35cc8b
Tanaka <t.khondowe@gmail.com>
2022-06-27 06:06:02 -0500

    Clean up component

--- src/Orders.js
+++ src/Orders.js
@@ -1,17 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { db } from './firebaseconf';
-import { useStateValue } from './StateProvider'
+import { useStateValue } from './StateProvider';
 import Order from './Order';
 
 function Orders() {
   const  [{ basket, user }, dispatch] = useStateValue();
-  const  [orders, setOrders] = useState([]) 
+  const  [orders, setOrders] = useState([]);
 
   useEffect(() => {
     if (user) {
       db
       .collection('users')
       .doc(user?.uid)
+      .collection('orders')
       .orderBy('created', 'desc')
       .onSnapshot(snapshot => (
         setOrders(snapshot.docs.map(doc => ({
